a452b4dc16741dca7eb2e3032cfdb29bedac127f
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2023-11-06 12:25:49 +0100

    remove default welcome message and hello world command

--- src/extension.ts
+++ src/extension.ts
@@ -9,21 +9,7 @@ import { commands } from './commands';
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "localcompletion" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'localcompletion.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from LocalCompletion!');
-    }
-  );
+  console.log('LocalCompletion is now active!');
 
   context.subscriptions.push(
     vscode.languages.registerInlineCompletionItemProvider(
@@ -32,8 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
     ),
     ...commands
   );
-
-  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
