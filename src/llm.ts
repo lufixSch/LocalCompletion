b59b5790fe8ca5cd438b1b89b7b51bd0b6719c9b
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2023-11-15 19:38:29 +0100

    add dedicated LLM class with more powerfull functions [WIP]

---
+++ src/llm.ts
@@ -0,0 +1,72 @@
+import OpenAI from 'openai';
+import { workspace } from 'vscode';
+
+export class LLM {
+  apiEndpoint = 'http://localhost:5001/v1';
+  client: OpenAI;
+
+  constructor() {
+    this.apiEndpoint = workspace
+      .getConfiguration('localcompletion')
+      .get('active_endpoint', this.apiEndpoint);
+
+    this.client = new OpenAI({
+      apiKey: 'NONE',
+      baseURL: this.apiEndpoint,
+    });
+  }
+
+  async getCompletion(
+    prompt: string,
+    stop: string[] = [],
+    temp: number | null = null,
+    max_tokens: number | null = null
+  ) {
+    return await this.client.completions.create({
+      model: 'NONE',
+      prompt,
+      stream: true,
+      temperature:
+        temp ||
+        workspace.getConfiguration('localcompletion').get('temperature'),
+      max_tokens:
+        max_tokens ||
+        workspace.getConfiguration('localcompletion').get('max_tokens'),
+      stop: [
+        ...stop,
+        ...workspace
+          .getConfiguration('localcompletion')
+          .get('stop_sequences', []),
+      ],
+    });
+  }
+
+  async getInstruct(
+    system: string,
+    prompt: string,
+    stop: string[] = [],
+    temp: number | null = null,
+    max_tokens: number | null = null
+  ) {
+    return await this.client.chat.completions.create({
+      model: 'NONE',
+      messages: [
+        { role: 'system', content: system },
+        { role: 'user', content: prompt },
+      ],
+      stream: true,
+      temperature:
+        temp ||
+        workspace.getConfiguration('localcompletion').get('temperature'),
+      max_tokens:
+        max_tokens ||
+        workspace.getConfiguration('localcompletion').get('max_tokens'),
+      stop: [
+        ...stop,
+        ...workspace
+          .getConfiguration('localcompletion')
+          .get('stop_sequences', []),
+      ],
+    });
+  }
+}
